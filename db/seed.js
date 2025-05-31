78dc638eaf0465367c64453a6cb191eaeaa18ce3
Abanta6 <dalexbanta95@gmail.com>
2025-05-31 00:54:43 -0500

    completed middleware, queries and api for requireUsers and updated app for token

--- db/seed.js
+++ db/seed.js
@@ -14,8 +14,11 @@ async function seed() {
     await createPlaylist("Playlist " + i, "lorem ipsum playlist description");
     await createTrack("Track " + i, i * 50000);
   }
-  for (let i = 1; i <= 15; i++) {
-    const playlistId = 1 + Math.floor(i / 2);
-    await createPlaylistTrack(playlistId, i);
+  for (let i = 1; i <= 3; i++) {
+    const user = await createUser("user" + i, "password");
+    await createPlaylist("playlist" + i, "description" + i, user.id);
+    for (let j = 0; j < 5; j++) {
+      await createPlaylistTrack(user.id, (i - 1) * 5 + j + 1);
+    }
   }
 }
